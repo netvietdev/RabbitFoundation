db865a39f4ca60b2d792b85ee4d9637d19c4d6a9
juanonsoftware <juanonsoftware@github.com>
2015-08-24 10:05:17 +0700

    Foundation 1.3.6

---
+++ Rabbit.Foundation/Data/DataItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Rabbit.Foundation.Data
+{
+    public class DataItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Misc { get; set; }
+    }
+}
---
+++ Rabbit.Foundation/Data/DataItemExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Rabbit.Foundation.Data
+{
+    public static class DataItemExtensions
+    {
+        public static bool TryGet(this IList<DataItem> items, string key, out DataItem result)
+        {
+            var count = items.Count(x => x.Key.Equals(key));
+
+            if (count == 0)
+            {
+                result = null;
+                return false;
+            }
+
+            if (count == 1)
+            {
+                result = items.Single(x => x.Key.Equals(key));
+                return true;
+            }
+
+            throw new InvalidDataException("There are more than one instances with the same key in the list");
+        }
+
+        public static DataItem Get(this IList<DataItem> items, string key)
+        {
+            return items.SingleOrDefault(x => x.Key.Equals(key));
+        }
+    }
+}
