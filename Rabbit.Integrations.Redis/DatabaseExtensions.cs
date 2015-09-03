25af3a0e1bad115b04869cc0c4ccdeefdd671440
juanonsoftware <juanonsoftware@github.com>
2015-09-03 11:52:50 +0700

    Redis extensions

---
+++ Rabbit.Integrations.Redis/DatabaseExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Rabbit.SerializationMaster;
+using StackExchange.Redis;
+using System;
+
+namespace Rabbit.Integrations.Redis
+{
+    public static class DatabaseExtensions
+    {
+        public static T Get<T>(this IDatabase database, RedisKey key) where T : class
+        {
+            var dataOnCache = database.StringGet(key);
+            if (dataOnCache.IsNull)
+            {
+                return default(T);
+            }
+
+            return ((string)dataOnCache).Deserialize<T>();
+        }
+
+        public static bool Set<T>(this IDatabase database, RedisKey key, T @object) where T : class
+        {
+            return database.StringSet(key, @object.Serialize());
+        }
+
+        public static bool Set<T>(this IDatabase database, RedisKey key, T @object, TimeSpan expiry) where T : class
+        {
+            return database.StringSet(key, @object.Serialize(), expiry);
+        }
+    }
+}
