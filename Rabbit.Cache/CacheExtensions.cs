53906d6058778b64a12dbc6a07468f8722ed632f
juanonsoftware <juanonsoftware@github.com>
2015-09-03 16:37:32 +0700

    Add cache

---
+++ Rabbit.Cache/CacheExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rabbit.Cache
+{
+    public static class CacheExtensions
+    {
+        public static T GetOrExecute<T>(this ICache cache, string key, Func<T> function) where T : class
+        {
+            var value = cache.Get<T>(key);
+            if (value != null)
+            {
+                return value;
+            }
+
+            return function();
+        }
+    }
+}
