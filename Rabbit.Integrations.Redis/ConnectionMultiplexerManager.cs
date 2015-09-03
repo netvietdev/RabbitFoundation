f70a4bf20b81fe5c9d01901665d012fd7f4e342a
juanonsoftware <juanonsoftware@github.com>
2015-09-03 16:48:12 +0700

    Add connection mananger

---
+++ Rabbit.Integrations.Redis/ConnectionMultiplexerManager.cs
@@ -0,0 +1,42 @@
+﻿using StackExchange.Redis;
+using System.Net;
+
+namespace Rabbit.Integrations.Redis
+{
+    public class ConnectionMultiplexerManager
+    {
+        private static ConnectionMultiplexer _connection;
+        private static readonly object LockObj = new object();
+
+        public static ConnectionMultiplexer GetCurrent(string endPoint, string password)
+        {
+            lock (LockObj)
+            {
+                if (_connection == null)
+                {
+                    InitializeConnection(endPoint, password);
+                }
+            }
+
+            return _connection;
+        }
+
+        private static void InitializeConnection(string endPoint, string password)
+        {
+            var datas = endPoint.Split(':');
+
+            var host = datas[0];
+            var port = int.Parse(datas[1]);
+
+            var cfg = new ConfigurationOptions()
+            {
+                Ssl = false,
+                Password = password,
+            };
+            cfg.EndPoints.Add(new DnsEndPoint(host, port));
+
+            _connection = ConnectionMultiplexer.Connect(cfg);
+        }
+
+    }
+}
