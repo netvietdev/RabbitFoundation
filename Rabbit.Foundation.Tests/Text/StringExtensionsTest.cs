80f16b1bfef661e03be226f41e453a03daf7a676
juanonsoftware <juanonsoftware@github.com>
2015-09-04 18:05:25 +0700

    Add string extension methods

---
+++ Rabbit.Foundation.Tests/Text/StringExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rabbit.Foundation.Text;
+
+namespace Rabbit.Foundation.Tests.Text
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void CanGetSubstring()
+        {
+            const string source = "This is a test string";
+
+            var substring = source.GetSubstring(3, " ");
+
+            Assert.AreEqual("This", substring);
+        }
+
+        [TestMethod]
+        public void CanGetSubstring2()
+        {
+            const string source = "This is a test string. And second string";
+
+            var substring = source.GetSubstring(21, new string[] { " ", "." });
+
+            Assert.AreEqual("This is a test string", substring);
+        }
+    }
+}
---
+++ Rabbit.Foundation/Text/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Rabbit.Foundation.Text
+{
+    public static class StringExtensions
+    {
+        public static string GetSubstring(this string source, int length)
+        {
+            return source.GetSubstring(length, ".");
+        }
+
+        public static string GetSubstring(this string source, int length, params string[] options)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return string.Empty;
+            }
+
+            if (source.Length <= length)
+            {
+                return source;
+            }
+
+            var indices =
+                options.Select(
+                    separator => source.IndexOf(separator, length, StringComparison.CurrentCultureIgnoreCase))
+                    .Where(index => index >= 0)
+                    .ToList();
+
+            if (indices.Count > 0)
+            {
+                return source.Substring(0, indices.Min());
+            }
+
+            return source;
+        }
+    }
+}
