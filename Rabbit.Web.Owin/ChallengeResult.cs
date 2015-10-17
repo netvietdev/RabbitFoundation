eab043d8effdf69401f4f5dcb5b196bf0c3aa838
juanonsoftware <huanhvhd@gmail.com>
2015-10-17 11:13:01 +0700

    Rabbit.Web.Owin 1.0.1

---
+++ Rabbit.Web.Owin/ChallengeResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Rabbit.Web.Owin
+{
+    public class ChallengeResult : IHttpActionResult
+    {
+        public ChallengeResult(string provider, HttpRequestMessage requestMessage)
+        {
+            AuthenticationProvider = provider;
+            RequestMessageMessage = requestMessage;
+        }
+
+        public string AuthenticationProvider { get; private set; }
+
+        public HttpRequestMessage RequestMessageMessage { get; private set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            RequestMessageMessage.GetOwinContext().Authentication.Challenge(AuthenticationProvider);
+
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+                RequestMessage = RequestMessageMessage
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ Rabbit.Web.Owin/ControllerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Rabbit.Web.Owin
+{
+    public static class ControllerExtensions
+    {
+        public static string GetControllerName(this ApiController controller)
+        {
+            return GetControllerName(controller.GetType().Name);
+        }
+
+        public static string GetControllerName(this Type apiControllerType)
+        {
+            if (typeof(ApiController).IsAssignableFrom(apiControllerType))
+            {
+                return GetControllerName(apiControllerType.Name);
+            }
+            return string.Empty;
+        }
+
+        private static string GetControllerName(string typeName)
+        {
+            return typeName.Substring(0, typeName.IndexOf("Controller", StringComparison.InvariantCulture));
+        }
+    }
+}
