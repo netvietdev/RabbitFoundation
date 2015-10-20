4e234657d87b8c640f1c8e90ac0659b4f4a6a730
juanonsoftware <juanonsoftware@github.com>
2015-10-20 11:16:26 +0700

    Rabbit.Web.Mvc version 1.1.1

---
+++ Rabbit.Web.Mvc/Security/SafeHtmlAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rabbit.Web.Mvc.Security
+{
+    /// <summary>
+    /// Apply on property which needs to be treated for safe string
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class SafeHtmlAttribute : Attribute
+    {
+    }
+}
---
+++ Rabbit.Web.Mvc/Security/SafeHtmlModelBinder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Security.Application;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Rabbit.Web.Mvc.Security
+{
+    /// <summary>
+    /// The binder with ability to handle SafeHtmlAttribute
+    /// </summary>
+    public class SafeHtmlModelBinder : DefaultModelBinder
+    {
+        protected override void SetProperty(ControllerContext controllerContext, ModelBindingContext bindingContext,
+            PropertyDescriptor propertyDescriptor, object value)
+        {
+            if (propertyDescriptor.PropertyType == typeof(string) && propertyDescriptor.Attributes.OfType<AllowHtmlAttribute>().Any())
+            {
+                value = TryGetSafeValue(value);
+            }
+
+            base.SetProperty(controllerContext, bindingContext, propertyDescriptor, value);
+        }
+
+        private static object TryGetSafeValue(object value)
+        {
+            var valueAsString = value as string;
+
+            if (!string.IsNullOrWhiteSpace(valueAsString))
+            {
+                value = Sanitizer.GetSafeHtmlFragment(valueAsString);
+            }
+
+            return value;
+        }
+    }
+}
