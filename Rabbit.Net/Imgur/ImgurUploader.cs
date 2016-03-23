e3b016623cf2028cb2d6c2aeff9f767033d94707
juanonsoftware <juanonsoftware@github.com>
2016-03-23 17:46:10 +0700

    Add Imgur uploader to Rabbit.Net

---
+++ Rabbit.Net/Imgur/ImgurUploader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Rabbit.Net.Imgur
+{
+    public class ImgurUploader
+    {
+        /// <summary>
+        /// Upload an image to imgur.com.
+        /// Just registering an application and call this method with the correct client ID
+        /// </summary>
+        public XDocument Upload(string clientId, ImageData image)
+        {
+            using (var w = new WebClient())
+            {
+                w.Headers.Add("Authorization", "Client-ID " + clientId);
+                var values = image.ToNameValueCollection();
+
+                byte[] response = w.UploadValues("https://api.imgur.com/3/upload.xml", values);
+                return XDocument.Load(new MemoryStream(response));
+            }
+        }
+    }
+}
---
+++ Rabbit.Net/Imgur/ImgurUploaderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Specialized;
+
+namespace Rabbit.Net.Imgur
+{
+    public static class ImgurUploaderExtensions
+    {
+        public static NameValueCollection ToNameValueCollection(this ImageData imageData)
+        {
+            var values = new NameValueCollection
+            {
+                {"image", imageData.Image}
+            };
+
+            if (!string.IsNullOrWhiteSpace(imageData.Album))
+            {
+                values.Add("album", imageData.Album);
+            }
+            if (!string.IsNullOrWhiteSpace(imageData.Type))
+            {
+                values.Add("type", imageData.Type);
+            }
+            if (!string.IsNullOrWhiteSpace(imageData.Name))
+            {
+                values.Add("name", imageData.Name);
+            }
+            if (!string.IsNullOrWhiteSpace(imageData.Title))
+            {
+                values.Add("title", imageData.Title);
+            }
+            if (!string.IsNullOrWhiteSpace(imageData.Description))
+            {
+                values.Add("description", imageData.Description);
+            }
+
+            return values;
+        }
+    }
+}
