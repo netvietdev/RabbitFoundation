13e4727ff475daef1b21b2787952ad64a86ed140
juanonsoftware <juanonsoftware@github.com>
2016-04-20 16:54:50 +0700

    Add assembly extension

---
+++ Rabbit.Foundation/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Rabbit.Foundation.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Get content from an embeded file
+        /// </summary>
+        public static string GetResourceFileContent(this Assembly assembly, string @namespace, string filePath)
+        {
+            var resourceName = @namespace + "." + filePath;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    return string.Empty;
+                }
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
