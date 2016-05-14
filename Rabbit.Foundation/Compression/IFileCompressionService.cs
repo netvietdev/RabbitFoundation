dfd4bb0e21ffbb1a7df91254fa156b0293be30be
juanonsoftware <huanhvhd@gmail.com>
2016-05-14 22:15:01 +0700

    Add FileCompressionService

---
+++ Rabbit.Foundation/Compression/IFileCompressionService.cs
@@ -0,0 +1,15 @@
+﻿namespace Rabbit.Foundation.Compression
+{
+    public interface IFileCompressionService
+    {
+        /// <summary>
+        /// Compresses a file and returns path of the new file (in the same directory with original file)
+        /// </summary>
+        string Compress(string filePath);
+
+        /// <summary>
+        /// Compresses a file to the given path
+        /// </summary>
+        void Compress(string filePath, string compressionPath);
+    }
+}
---
+++ Rabbit.Foundation/Compression/NetFxFileCompressionService.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Rabbit.Foundation.Compression
+{
+    /// <summary>
+    /// Use dotnet internal functions for compression
+    /// </summary>
+    public class NetFxFileCompressionService : IFileCompressionService
+    {
+        public string Compress(string filePath)
+        {
+            var archiveFileName = Path.ChangeExtension(filePath, ".zip");
+
+            using (var archive = ZipFile.Open(archiveFileName, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+
+            return archiveFileName;
+        }
+
+        public void Compress(string filePath, string compressionPath)
+        {
+            using (var archive = ZipFile.Open(compressionPath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+        }
+    }
+}
