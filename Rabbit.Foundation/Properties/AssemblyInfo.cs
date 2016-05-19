d740365bc2e7698063cdf80dbc63ca1e57efc358
juanonsoftware <juanonsoftware@github.com>
2016-05-19 18:22:14 +0700

    Signed Rabbit.Foundation

--- Rabbit.Foundation/Properties/AssemblyInfo.cs
+++ Rabbit.Foundation/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -36,3 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
+[assembly: AssemblyKeyFile(@"..\..\..\BitBucket\SigningKeys\Rabbit.snk")]
