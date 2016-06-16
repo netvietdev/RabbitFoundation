84afce76cae409874fcc7abe1375ca0b0db2b4cf
juanonsoftware <juanonsoftware@github.com>
2016-06-16 18:04:58 +0700

    Add collection methods

---
+++ Rabbit.Foundation.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rabbit.Foundation.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rabbit.Foundation.Tests
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        [TestMethod]
+        public void CanParseLinesToDictionary()
+        {
+            var lines = new List<string>
+            {
+                "Line1 = A1", 
+                "Line2 = A2"
+            };
+
+            var dict = lines.ToDictionary();
+
+            Assert.AreEqual(2, dict.Count);
+            Assert.AreEqual(true, dict.ContainsKey("Line1"));
+            Assert.AreEqual("A1", dict.First().Value);
+            Assert.AreEqual(true, dict.ContainsKey("Line2"));
+            Assert.AreEqual("A2", dict.Last().Value);
+        }
+    }
+}
--- Rabbit.Foundation/Extensions/CollectionExtensions.cs
+++ Rabbit.Foundation/Extensions/CollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
 
 namespace Rabbit.Foundation.Extensions
 {
@@ -18,6 +20,28 @@ namespace Rabbit.Foundation.Extensions
             ICollection<KeyValuePair<string, object>> someObject = new ExpandoObject();
             someObject.AddRange(source);
             return someObject;
+        }
+
+        /// <summary>
+        /// Convert a list of lines of string to dictionary. Take the first equal sign to separate the key and value
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when a line does not contain equal sign</exception>
+        public static IDictionary<string, string> ToDictionary(this IEnumerable<string> lines)
+        {
+            var result = new Dictionary<string, string>();
+
+            foreach (var line in lines.Where(x => !string.IsNullOrWhiteSpace(x)))
+            {
+                if (!line.Contains("="))
+                {
+                    throw new ArgumentException(string.Format("The line '{0}' must contain at least one equal sign", line));
+                }
+
+                var parts = line.Split(new[] { '=' }, 2);
+                result.Add(parts[0].Trim(), parts[1].Trim());
+            }
+
+            return result;
         }
     }
 }
