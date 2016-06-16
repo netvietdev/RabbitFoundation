1942082f62f8d74f13b2118e2caad283bc13a871
juanonsoftware <juanonsoftware@github.com>
2016-06-16 17:42:36 +0700

    Added collection extension methods

---
+++ Rabbit.Foundation/Extensions/CollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Rabbit.Foundation.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                collection.Add(item);
+            }
+        }
+
+        public static object ToDynamicObject(this IDictionary<string, object> source)
+        {
+            ICollection<KeyValuePair<string, object>> someObject = new ExpandoObject();
+            someObject.AddRange(source);
+            return someObject;
+        }
+    }
+}
