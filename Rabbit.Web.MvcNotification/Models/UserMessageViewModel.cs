911683a4f7de69b5b02153b23ddf1a5f681e390d
juanonsoftware <juan@vndev.top>
2018-11-24 22:21:36 +0700

    Update message model object

--- Rabbit.Web.MvcNotification/Models/UserMessageViewModel.cs
+++ Rabbit.Web.MvcNotification/Models/UserMessageViewModel.cs
@@ -5,5 +5,15 @@
         public string Title { get; set; }
 
         public string Message { get; set; }
+
+        /// <summary>
+        /// The action URL for Confirm/Next button
+        /// </summary>
+        public string NextAction { get; set; }
+
+        /// <summary>
+        /// The action URL for Cancel/Back button
+        /// </summary>
+        public string BackAction { get; set; }
     }
 }
